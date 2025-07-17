5c4d0650d58e86d10a535297ba8f3f78d031f323
Arnavbgl <arnavbaghel7299@gmail.com>
2025-07-18 00:07:08 +0530

    second commit

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 const parent = React.createElement("div", {id:"parent" }, React.createElement("div", {id:"child" },React.createElement("h1", {id:"heading" }, "heading1")));
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(parent); 
