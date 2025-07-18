a7746971d10c793f0209df1af115aac686674131
Arnavbgl <arnavbaghel7299@gmail.com>
2025-07-18 21:23:52 +0530

    upgrade

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+
 const parent = React.createElement("div", {id:"parent" }, React.createElement("div", {id:"child" },React.createElement("h1", {id:"heading" }, "heading1")));
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent); 
