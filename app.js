4b49f5cf8943b7fd8767324c22ada1857e86e310
Arnavbgl <arnavbaghel7299@gmail.com>
2025-07-25 19:15:01 +0530

    new learning

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// JSX  - it is html / xml like syntax
+const jsxheading = (<h1>
+    hello from jsx
+    </h1>);
+
+// components
 
 
+const root =ReactDOM.createRoot(document.getElementById("root"));
 
-const hello = React.createElement("div", {}, "hello i am new here");
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(hello); 
+root.render(jsxheading);
